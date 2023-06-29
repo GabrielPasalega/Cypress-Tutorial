b2f0cd9304520a149ad5253e3395f45987136afd
GabrielPasalega <PasalegaGabriel77@gmail.com>
2023-06-29 16:51:06 +0300

    SQL and Excel updates

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,13 +5,16 @@ const {isFileExist, findFiles} = require ('cy-verify-downloads')
 const xlsx = require('node-xlsx').default;
 const fs = require('fs')//for file
 const path = require('path')
+//mySQL requirements
+const mysql = require("mysql")
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://uitestingplayground.com",
     setupNodeEvents(on, config) {
+  //Verify download import    
       on('task', {isFileExist,findFiles})
-
+//Excel
       on("task",{
         parseXlsx({filePath}){
           return new Promise((resolve,reject)=>{
@@ -23,7 +26,13 @@ export default defineConfig({
             }
           })
         }
-      })
+      }),
+      //mySQL implementation
+      on("task", {
+        queryDb: (query) => {
+          return queryTestDb(query, config);
+        },
+      });
       require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
@@ -32,7 +41,13 @@ export default defineConfig({
       demoQA: "https://demoqa.com/",
       theInternet: "https://the-internet.herokuapp.com/",
       //https://www.globalsqa.com/angularjs-protractor-practice-site/
-      Angular: "https://www.globalsqa.com/"
+      Angular: "https://www.globalsqa.com/",
+      db:{
+        host: "localhost",
+        user:"root",
+        password:"",
+        database:"cypress_test"
+      }
     }
   },
   pageLoadTimeout:60000,
@@ -51,3 +66,21 @@ export default defineConfig({
     openMode:1
   }
 });
+
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db);
+  // start connection to db
+  connection.connect();
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) reject(error);
+      else {
+        connection.end();
+        // console.log(results)
+        return resolve(results);
+      }
+    });
+  });
+}
